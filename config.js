346cbe4c5128187d32713ae9eaba43ba9a34c126
maddie <madie595cp@gmail.com>
2018-06-16 22:43:08 -0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'D0ayPOZwV9yuvLDrXFvwhwJKi',
+  consumer_secret:      'pV229ZHQgzcxYgnZYtgRBs9tLhS4bL6nDECntnz8TFaB4NPzCg',
+  access_token:         '860275248222044161-YDZSrw9Dzyr4u4a1eBhCuWbmCMytUiX',
+  access_token_secret:  '7YdF4B2myMJvHXEq7uZwCpdyGXPFMTBcxRgjEigWlYKtN'
+}
+
+module.exports = config;
